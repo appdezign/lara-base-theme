543033b348ef679ceb6d7155af564819607c8893
Sybrand Hoeksma <s.hoeksma@firmaq.nl>
2025-09-01 13:25:19 +0200

    Small fixes

--- views/_widgets/menu-level-one/top.blade.php
+++ views/_widgets/menu-level-one/top.blade.php
@@ -1,11 +1,9 @@
-<?php
-$lang = LaravelLocalization::getCurrentLocale();
-$navlink = ($node->type == 'url') ? $node->url : url($lang . '/' . $node->route);
-?>
+<?php $lang = LaravelLocalization::getCurrentLocale(); ?>
 
 <ul class="navbar-nav flex-row me-16">
 	@foreach($menulevelone as $item)
 		@if($item->publish == 1)
+			<?php $navlink = ($item->type == 'url') ? $item->url : url($lang . '/' . $item->route); ?>
 			<li class="nav-item">
 				<a href="{{ $navlink }}"
 				   class="nav-link fs-14 px-8 py-4">
